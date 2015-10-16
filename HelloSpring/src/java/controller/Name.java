57b8918acd0ef230d93e3218f827249d9354e4b0
LabGSG-15 <LabGSG-15@PC-15>
2015-10-16 09:07:02 +0700

    ubah nameView

---
+++ HelloSpring/src/java/controller/Name.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author LabGSG-15
+ */
+public class Name {
+    private String value;
+    private String nim;
+    
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public String getNim(){
+        return nim;
+    }
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+}
---
+++ HelloSpring/src/java/service/HelloService.java
@@ -0,0 +1,17 @@
+package service;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LabGSG-15
+ */
+public class HelloService {
+    public static String sayHello(String name, String nim){
+        return "Hello "+name+", Nim anda : "+nim+"!";
+    }
+}
